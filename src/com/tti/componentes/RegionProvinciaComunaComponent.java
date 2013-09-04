01c3eeaf4edbee55661450456c0344fd6e4280c5
José Hurtado <mymail@mail.com>
2013-09-04 02:00:49 -0400

    No funcan listeners combobox

--- src/com/tti/componentes/RegionProvinciaComunaComponent.java
+++ src/com/tti/componentes/RegionProvinciaComunaComponent.java
@@ -4,9 +4,16 @@ import java.util.List;
 
 import com.tti.data.ApiDPA;
 import com.tti.entidad.Sector;
+import com.vaadin.data.Item;
+import com.vaadin.data.Property.ValueChangeEvent;
+import com.vaadin.data.Property.ValueChangeListener;
+import com.vaadin.data.util.BeanItemContainer;
+import com.vaadin.ui.AbstractSelect.ItemCaptionMode;
+import com.vaadin.ui.ComboBox;
 import com.vaadin.ui.CustomComponent;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.NativeSelect;
+import com.vaadin.ui.Notification;
 import com.vaadin.ui.VerticalLayout;
 
 public class RegionProvinciaComunaComponent extends CustomComponent{
@@ -14,8 +21,8 @@ public class RegionProvinciaComunaComponent extends CustomComponent{
 	private static final long serialVersionUID = -7034670941893640320L;
 	private static ApiDPA apiDpaConnection;
 	private NativeSelect comboRegion;
-	private NativeSelect comboProvincia;
-	private NativeSelect comboComuna;
+	private ComboBox comboProvincia;
+	private ComboBox comboComuna;
 	private HorizontalLayout horizontalLayout;
 	private VerticalLayout verticalLayout;
 	
@@ -26,8 +33,8 @@ public class RegionProvinciaComunaComponent extends CustomComponent{
 	
 	private void initLayout(boolean horizontal){
 		comboRegion = new NativeSelect("Región");
-		comboProvincia = new NativeSelect("Provincia");
-		comboComuna = new NativeSelect("Comuna");
+		comboProvincia = new ComboBox("Provincia");
+		comboComuna = new ComboBox("Comuna");
 		
 		if(horizontal){
 			horizontalLayout = new HorizontalLayout(comboRegion, comboProvincia, comboComuna);
@@ -39,15 +46,21 @@ public class RegionProvinciaComunaComponent extends CustomComponent{
 				
 	}
 	
+	@SuppressWarnings("unchecked")
 	private void setDataSource(){
 		apiDpaConnection = new ApiDPA();
 		List<Sector> listaComunas = apiDpaConnection.getComunas("", "", "");
 		List<Sector> listaProvincias = apiDpaConnection.getProvincias("", "");
 		List<Sector> listaRegiones = apiDpaConnection.getRegiones("");
 		
-		for (Sector region : listaRegiones) {
-			comboRegion.addItem(region.getNombre());
-		}
+		BeanItemContainer<Sector> containerRegiones = new BeanItemContainer(Sector.class, listaRegiones);
+		comboRegion.setContainerDataSource(containerRegiones);
+		comboRegion.setItemCaptionMode(ItemCaptionMode.PROPERTY);
+		comboRegion.setItemCaptionPropertyId("nombre");
+//		for (Sector region : listaRegiones) {
+//			Item item = comboRegion.addItem(region.getCodigo());
+//			item.getItemProperty("name").setValue(region.getNombre());
+//		}
 		
 		for (Sector provincia : listaProvincias) {
 			comboProvincia.addItem(provincia.getNombre());
@@ -56,5 +69,18 @@ public class RegionProvinciaComunaComponent extends CustomComponent{
 		for (Sector comuna : listaComunas) {
 			comboComuna.addItem(comuna.getNombre());			
 		}
+	}
+	
+	private void setListeners(){
+		
+		comboRegion.addValueChangeListener(new ValueChangeListener() {
+			
+			@Override
+			public void valueChange(ValueChangeEvent event) {
+				System.out.println("Valor: " + event.getProperty().getValue());
+				Notification.show("Valor: " + event.getProperty().getValue());
+				
+			}
+		});
 	}
 }
